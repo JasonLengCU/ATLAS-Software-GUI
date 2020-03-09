5ca4242ca40a269e1b28cffd76e2084d8fb19274
CMacCraiger <charles.maccraiger@colorado.edu>
2020-03-09 01:28:32 -0600

    socket sending

--- LimSwitchImposter.py
+++ LimSwitchImposter.py
@@ -3,11 +3,11 @@ import socket
 host = "localhost"
 port = 5556
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-try:
-    s.bind((host, port))
-    print('Outgoing Port Bound')
-except socket.error as e:
-    print(str(e))
+# try:
+#     s.bind((host, port))
+#     print('Outgoing Port Bound')
+# except socket.error as e:
+#     print(str(e))
 
 cmdout = '111'
 while 1:
